627385c0b58619f871081c242240f1d463894173
Matthew LeFevre <matthewlefevre95@gmail.com>
2017-04-05 19:51:59 -0600

    lots of added svgs and general edits

--- bs.js
+++ bs.js
@@ -24,9 +24,36 @@ window.onload = function () {
 
     },
 
-
   };
 
+  var carousel = {
+    c: $('#c'),
+    imgs: ['https://static.pexels.com/photos/63596/fruit-pepper-vegetable-vegetables-63596.jpeg', 'https://static.pexels.com/photos/238140/pexels-photo-238140.jpeg', 'https://static.pexels.com/photos/40120/pexels-photo-40120.jpeg', 'https://static.pexels.com/photos/59523/pexels-photo-59523.jpeg'],
+    nextimg: 0,
+    doSlideshow: function () {
+      if(carousel.nextimg >= carousel.imgs.length){carousel.nextimg = 0};
+      carousel.c
+        .css('background-image', 'url("'+carousel.imgs[carousel.nextimg++]+'")')
+        .fadeIn(1000, function(){
+          setTimeout(carousel.doSlideshow, 5000);
+      })
+    }
+  }
+
+
+
+//  function doSlideshow() {
+//    if(nextimg>=imgs.length){nextimg=0;}
+//    $(car.el)
+//      .css('background-image', 'url("'+imgs[nextimg++]+'")')
+//      .fadeIn(1000,function(){
+//        setTimeout(doSlideshow,5000);
+//      });
+//  }
+
+
+
+  carousel.doSlideshow();
   nav.simple_drop();
   nav.bottom_drop();
   };
---
+++ partials/generals/README.md
@@ -0,0 +1,5 @@
+# BS Generals
+
+In a nutshell generals are styles that effect the overall website and not just small sections. They have to do with default fonts as well as the box-model.
+
+BS Generals should be included in all web projects.
