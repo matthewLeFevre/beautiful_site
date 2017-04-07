c4d44335987ce7f717e7afe8249bf6502e54c988
Matthew LeFevre <matthewlefevre95@gmail.com>
2017-04-07 10:59:44 -0600

    Launched version 1.0 of BS

--- bs.js
+++ bs.js
@@ -28,14 +28,17 @@ window.onload = function () {
 
   var carousel = {
     c: $('#c'),
+    ch: $('#ch'),
     imgs: ['https://static.pexels.com/photos/63596/fruit-pepper-vegetable-vegetables-63596.jpeg', 'https://static.pexels.com/photos/238140/pexels-photo-238140.jpeg', 'https://static.pexels.com/photos/40120/pexels-photo-40120.jpeg', 'https://static.pexels.com/photos/59523/pexels-photo-59523.jpeg'],
+    text: ['build your place on the web', 'find joy in your surroundings', 'discover things you never knew', 'Make friends along the way'],
     nextimg: 0,
     doSlideshow: function () {
       if(carousel.nextimg >= carousel.imgs.length){carousel.nextimg = 0};
+        carousel.ch.html(carousel.text[carousel.nextimg])
       carousel.c
         .css('background-image', 'url("'+carousel.imgs[carousel.nextimg++]+'")')
         .fadeIn(1000, function(){
-          setTimeout(carousel.doSlideshow, 5000);
+          setTimeout(carousel.doSlideshow, 4000);
       })
     }
   }
