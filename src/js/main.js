9950518e57d480f7cb7a88da7506401549cc14aa
Matthew Robert LeFevre <matthewlefevre@ldschurch.org>
2017-09-20 16:26:58 -0600

    JavaScript change text styles format

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,66 @@ Render it back to the user
 */
 window.onload = function () {
   
+  var uploadImage = {
+//    self: uploadImage,
+    imgElGroup: [],
+    imgInput:     document.getElementsByName("imgIn")[0],
+    imgOutput:    document.getElementsByTagName("main")[0],
+    clickImages:  document.getElementsByClassName('js-upload--img'),
+    imgInputFile,
+    imgEl: " ",
+    
+    clickableImages: function () {
+      for (var i = 0; i < uploadImage.clickImages.length; i++) {
+        uploadImage.clickImages[i].addEventListener("click", function () {
+          uploadImage.clickImager[i].classList.toggle('upload--img__blowup');
+        });
+      }
+    },
+    
+    removeExistingImages: function (images) {
+      var id, image;
+      for (var i = 0; i < images.length; i++) {
+        id = "imgNum" + i;
+        image = document.getElementById(id);
+        uploadImage.imgOutput.removeChild(image);
+        uploadImage.imgElGroup = [];
+      }
+    },
+    
+    convertUrl: function(item) {
+      return URL.createObjectURL(item)
+    },
+    
+   
+    renderImgFromInput: function () {
+//      var imgEl;
+      uploadImage.imgInput.addEventListener("change", function () {
+        uploadImage.removeExistingImages(uploadImage.imgElGroup);
+        for (var i = 0; i < uploadImage.imgInput.files.length; i++) {
+          uploadImage.imgEl = document.createElement("img");
+          uploadImage.imgInputFile = uploadImage.imgInput.files[i];
+          uploadImage.imgEl.src = uploadImage.convertUrl(uploadImage.imgInputfile);
+          uploadImage.imgEl.setAttribute("id", "imgNum" + i);
+          uploadImage.imgEl.setAttribute("class", "upload--img js-upload--img");
+          uploadImage.imgElGroup.push(uploadImage.imgEl);
+          uploadImage.imgOutput.appendChild(uploadImage.imgEl);
+        }
+      });
+    },
+    
+    ready: function () {
+      uploadImage.renderImgFromInput();
+    }
+  }
+  
   var imgElGroup = [],
     imgInput = document.getElementsByName("imgIn")[0],
     imgOutput = document.getElementsByTagName("main")[0],
     imgInputFile;
   
-  var clickableImages = function () {
+  function clickableImages () {
     var clickImages = document.getElementsByClassName('js-upload--img');
-    console.log("fireing", clickImages);
     for (var i = 0; i < clickImages.length; i++){
       clickImages[i].addEventListener("click", function () {
         this.classList.toggle('upload--img__blowup');
@@ -24,16 +76,11 @@ window.onload = function () {
   
   function removeExistingImages(images) {
     var id, image;
-    console.log(imgElGroup, "fireing");
     for (var i = 0; i < images.length; i++) {
         id = "imgNum" + i;
-      console.log(id);
         image = document.getElementById(id);
-      console.log(image);
-      console.log("before", imgElGroup);
         imgOutput.removeChild(image);
         imgElGroup = [];
-      console.log("after", imgElGroup);
       }
   }
   
@@ -50,15 +97,77 @@ window.onload = function () {
         imgOutput.appendChild(imgEl);
       }
       clickableImages();
-      console.log(imgInput.files);
     });
   };
   
-  
+
   
   
   renderImgFromInput();
+//  uploadImage.ready();
+  
+  function createCSSClass (className, css) {
+    var sallyStyles = document.getElementsByName("sallyStyles")[0];
+    if(sallyStyles !== undefined){
+      document.getElementsByTagName("head")[0].removeChild(sallyStyles);
+    }
+   
+   var style = document.createElement("style");
+    style.type= "text/css";
+    style.setAttribute("name", "sallyStyles");
+    style.innerHTML = "." + className + "{" + css + "}";
+    document.getElementsByTagName("head")[0].appendChild(style);
+  }
+ 
+  
+  var typeFormat = {
+    submit: document.getElementsByName("btn")[0],
+    typeFamily: " ",
+    typeColor: " ",
+    typeOutput: document.getElementsByClassName("js-type--format")[0],
+//    typeOutput_text: this.typeOutput.value,
+    
+    ready: function () {
+      this.submit.addEventListener("click", this.typeData);
+    },
+    
+    typeData: function() {
+      this.typeFamily = document.getElementsByName("type-family")[0].value;
+      this.typeColor = document.getElementsByName("type-color")[0].value;
+      console.log("typeData");
+      if(this.typeFamily === null && this.typeColor === null){
+        
+        if(this.typeFamily === null) {
+          window.alert('you need to fill in this field');
+        } else {
+          var family = "font-family: " + this.typeFamily + ";";
+          typeFormat.makeCSS(family);
+        }
+
+        if(this.typeColor === null) {
+          window.alert('you need to fill in this field');
+        } else {
+          var color ="color: " + this.typeColor + ";";
+          typeFormat.makeCSS(color);
+        } 
+      } else {
+        var family = "font-family: " + this.typeFamily + ";";
+        var color ="color: " + this.typeColor + ";";
+        typeFormat.makeCSS(family + " " + color);
+      }
+    },
+    
+    makeCSS: function (css) {
+      createCSSClass("harvey", css);
+      this.typeOutput.setAttribute("class", "harvey");
+    }
+    
+  }
+  
+  typeFormat.ready();
 };
+
+ 
 
 
 
